5da0a6b17c314aea9774b5835ae6492accd84cab
camilopulgarin <kmilo-1997-18@hotmail.com>
2020-10-21 09:15:28 -0500

    CRUD

---
+++ src/app/components/App.vue
@@ -0,0 +1,166 @@
+<template>
+    <div>
+        <nav class="navbar navbar-ligth bg-light">
+        <a href="/" class="navbar-brand">MEVN Stack</a>
+
+    </nav>
+
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
+                        <form @submit.prevent="addTask">
+                            <div class="form-group">
+                            <input type="text" v-model="task.title"
+                            placeholder="Insert a Task" 
+                            class="form-control">
+                            </div >
+                            <div class="form-group">
+                                <textarea v-model="task.description"
+                                cols="30" rows="10" 
+                                class="form-control" placeholder="Insert A Description"></textarea>
+                            </div>
+                            <template v-if="edit === false">
+                                <button class="btn btn-primary btn-block"
+                                >Send</button>
+                            </template>
+                            <template v-else>
+                                <button class="btn btn-primary btn-block">
+                                    Update</button>
+                            </template>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="task of tasks">
+                            <td>{{task.title}}</td>
+                            <td>{{task.description}}</td>
+                            <td>
+                                <button @click="deleteTask(task._id)"
+                                class="btn btn-danger">
+                                    Delete
+                                </button>
+                                <button @click="editTask(task._id)"
+                                class="btn btn-secondary">
+                                    Edit
+                                </button>
+                            </td>
+
+                        </tr>
+                    </tbody>
+
+                </table>
+            </div>
+    </div>
+    </div>
+    </div>
+</template>
+
+<script>
+
+   class Task{
+       constructor(title, description){
+           this.title = title;
+           this.description = description;
+       }
+        
+    }
+
+export default {
+    data() {
+        return {
+            task: new Task(),
+            tasks:[],
+            edit: false,
+            taskToEdit:''
+        }
+    },
+    created(){
+        this.getTasks();
+    },
+    methods: {
+        addTask(){
+            if(this.edit === false){
+                fetch('/api/tasks', {
+                    method: 'POST',
+                    body: JSON.stringify(this.task),
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-type': 'application/json'
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        this.getTasks();
+                    })
+            } else{
+                fetch('/api/tasks/'+ this.taskToEdit, {
+                    method: 'PUT',
+                    body: JSON.stringify(this.task),
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-type': 'application/json'
+                    }
+                })
+                .then(res => res.json())
+                .then(data => {
+                    this.getTasks();
+                    this.edit = false;
+                });
+            }
+
+
+            this.task = new Task();
+        },
+        getTasks(){
+            fetch('/api/tasks')
+                .then(res => res.json())
+                .then(data => {
+                    this.tasks = data;
+                    console.log(this.tasks);
+            });
+
+        },
+        deleteTask(id){
+            console.log(id);
+           fetch('/api/tasks/'+ id, {
+                method:'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                this.getTasks();
+            });
+        },
+
+        editTask(id){
+            fetch('/api/tasks/'+ id)
+                .then(res => res.json())
+                .then(data => {
+                     this.task = new Task(data.title, data.description);
+                     this.taskToEdit = data._id;
+                     this.edit = true;
+                })
+                
+                /* {
+                    this.task = new Task(data.title, data.description);
+                });*/
+
+        }
+    }
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+mongoose.connect('mongodb://localhost/mevn-database')
+    .then(db => console.log('DB conectada'))
+    .catch(err => console.error(err));
+
+//Setings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks', require('./routes/task'));
+//Static files
+app.use(express.static(__dirname+'/public'));
+//Server is listening
+app.listen(app.get('port'), () => {
+    console.log('server on port ', app.get('port'));
+});
